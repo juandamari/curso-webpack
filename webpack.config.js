ee5a39ecf281fb6b5b73c1750d28038aa242367a
juan david M <juan_da-mari@hotmail.com>
2022-07-25 22:30:24 -0500

    cambiando fuentes, iamgenes y optminizando

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');//agregando el plugin webpack con html
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');//agregando el plugin de css mini
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]',
     },
     resolve: {
         extensions: ['.js']
@@ -25,6 +27,25 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader,
                 'css-loader',
                 'stylus-loader'], //el "use" se puede usar con un objeto {} o un arreglo []
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    },
+                }
             }
         ]
     },
@@ -34,6 +55,13 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }), //es configuracion del plugin de html
-        new MiniCssExtractPlugin(),    
+        new MiniCssExtractPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, "src", "assets/images"),
+                to: "assets/images"
+            }
+            ]
+        })
     ]
 }
