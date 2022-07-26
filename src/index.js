e2ad84f097bfd5397c7499e5211914a0a1c5f6e9
juan david M <juan_da-mari@hotmail.com>
2022-07-25 21:20:24 -0500

    merge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import Template from './templates/Template.js';
-<<<<<<< HEAD
-console.log('hola');
-=======
 import './styles/main.css';
 import './styles/vars.styl';
->>>>>>> f335607d5ce186781488a6ea43ee2736e57de247
 
 (async function App() {
   const main = null || document.getElementById('main');
